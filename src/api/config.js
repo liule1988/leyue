06df495d43a1d78bb1b9b239d94ddb498389509d
778252899@qq.com <19930224ll>
2017-05-17 15:35:46 +0800

    delete

--- src/api/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  // site: 'https://bird.ioliu.cn/v1?url=',
-  // method: 'http://10.100.61.8:3000'
-    baseUrl:'http://localhost:3000'
-};
-export const API_ROOT = config.baseUrl;
